5f0f2e2db09e8d9868bffee5b6af21ed8de74ea5
DovzhenkoDenys1989 <ddv10jan1989@gmail.com>
2021-07-19 15:08:30 +0300

    Correction Practice-SQL HW20210715

--- sql/Practice-SQL HW20210715.sql
+++ sql/Practice-SQL HW20210715.sql
@@ -1,58 +1,41 @@
-/*
-1. Посчитать кол-во телефонов, которые были проданы
-2. Кол-во телефонов, которые есть "на складе"
-3. Средняя цена всех телефонов
-4. Средняя цена каждого бренда
-5. Кол-во моделей каждого бренда
-6. Кол-во заказов каждого пользователя, которые совершали заказы
-7. Средняя цена на IPhone
-8. Стоимость всех телефонов в диапазоне их цен от 10К до 20К
-9.  Узнать каких брендов телефонов осталось меньше всего
-10. Сортировка юзеров по возрасту(не день рождения) и по имени
-JOIN:
-11. Извлечь все телефоны заказа №4
-12. Кол-во заказов каждого пользователя и его емейл
-13. Извлечь самый популярный телефон (каких моделей телефонов заказано больше всего) 
-*/
-
-/*1*/
+/*1. Посчитать кол-во телефонов, которые были проданы*/
 SELECT SUM("quantity") AS "Проданные телефоны"
 FROM "phones_to_orders";
-/*2*/
+/*2. Кол-во телефонов, которые есть "на складе"*/
 SELECT SUM("quantity") AS "Телефоны на складе"
 FROM "phones";
-/*3*/
+/*3. Средняя цена всех телефонов*/
 SELECT AVG("price")
 FROM "phones";
-/*4*/
+/*4. Средняя цена каждого бренда*/
 SELECT "brand", AVG("price")
 FROM "phones"
 GROUP BY "brand";
-/*5*/
+/*5. Кол-во моделей каждого бренда*/
 SELECT "brand", "model", SUM("quantity")
 FROM "phones"
 GROUP BY "brand", "model"
 ORDER BY "brand", "model";
-/*6*/
+/*6. Кол-во заказов каждого пользователя, которые совершали заказы*/
 SELECT "userId", COUNT("id")
 FROM "orders"
 GROUP BY "userId"
 ORDER BY "userId";
-/*7*/
+/*7. Средняя цена на IPhone*/
 SELECT AVG("price")
 FROM "phones"
 WHERE "brand" = 'Iphone';
-/*8*/
-SELECT *
+/*8. Стоимость всех телефонов в диапазоне их цен от 10К до 20К*/
+SELECT SUM("price")
 FROM "phones"
 WHERE "price" > 10000 AND "price" < 20000;
-/*9*/
-SELECT "brand", MIN("quantity") AS "quantity"
+/*9.  Узнать каких брендов телефонов осталось меньше всего*/
+SELECT "brand", SUM("quantity") AS "quantity"
 FROM "phones"
 GROUP BY "brand"
 ORDER BY "quantity"
 LIMIT 5;
-/*10*/
+/*10. Сортировка юзеров по возрасту(не день рождения) и по имени*/
 SELECT "Age", "firstname"
 FROM (
   SELECT EXTRACT(
@@ -64,11 +47,12 @@ FROM (
 ) AS "users_age"
 GROUP BY "Age", "firstname"
 ORDER BY "Age", "firstname";
-/*11*/
+/*JOIN:
+11. Извлечь все телефоны заказа №4*/
 SELECT * FROM "phones" AS p
 JOIN "phones_to_orders" AS pto ON p."id" = pto."phoneId"
 WHERE pto."orderId" = 4;
-/*12*/
+/*12. Кол-во заказов каждого пользователя и его емейл*/
 SELECT u."id",
   u."email",
   COUNT(o."id") AS "ordersCount"
@@ -76,7 +60,7 @@ FROM users AS u
   LEFT JOIN orders AS o ON u."id" = o."userId"
 GROUP BY u."id",
   u."email";
-/*13*/
+/*13. Извлечь самый популярный телефон (каких моделей телефонов заказано больше всего)*/
 SELECT p.* FROM "phones" AS p
 JOIN "phones_to_orders" AS pto ON p."id" = pto."phoneId"
 ORDER BY pto."quantity" DESC
